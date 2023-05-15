b8a3f39e1f8673771c37a2d943af1a5b8bbabe4d
froggie vega <88820190+donkeybong@users.noreply.github.com>
2023-05-15 01:56:11 -0700

    Add files via upload

---
+++ data/animationStates.js
@@ -0,0 +1,50 @@
+let animationStates = [
+    {
+        name:"idle south",
+        frames: "2",
+    },
+    {
+        name:"walk south",
+        frames: "6",
+    },
+    {
+        name:"run south",
+        frames: "4",
+    },
+    {
+        name:"idle west",
+        frames: "2",
+    },
+    {
+        name:"walk west",
+        frames: "6",
+    },
+    {
+        name:"run west",
+        frames: "4",
+    },
+    {
+        name:"idle east",
+        frames: "2",
+    },
+    {
+        name:"walk east",
+        frames: "6",
+    },
+    {
+        name:"run east",
+        frames: "4",
+    },
+    {
+        name:"idle north",
+        frames: "2",
+    },
+    {
+        name:"walk north",
+        frames: "6",
+    },
+    {
+        name:"run north",
+        frames: "4",
+    },
+];
